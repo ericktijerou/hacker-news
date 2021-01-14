c47e62922fb8855ce2252b7e78b76d390cee7ae5
erick tijero <ericktijerou@gmail.com>
2021-01-14 09:41:03 -0500

    Fix data in release apk

--- app/src/main/kotlin/com/ericktijerou/hackernews/data/network/entity/BaseResponse.kt
+++ app/src/main/kotlin/com/ericktijerou/hackernews/data/network/entity/BaseResponse.kt
@@ -1,14 +1,33 @@
 package com.ericktijerou.hackernews.data.network.entity
 
+import com.google.gson.annotations.SerializedName
+
 open class BaseResponse<T> {
+    @SerializedName("hits")
     var hits: T? = null
+
+    @SerializedName("nbHits")
     var nbHits: Int? = null
+
+    @SerializedName("page")
     var page: Int? = null
+
+    @SerializedName("nbPages")
     var nbPages: Int? = null
+
+    @SerializedName("hitsPerPage")
     var hitsPerPage: Int? = null
+
+    @SerializedName("exhaustiveNbHits")
     var exhaustiveNbHits: Boolean? = null
+
+    @SerializedName("query")
     var query: String? = null
+
+    @SerializedName("params")
     var params: String? = null
+
+    @SerializedName("processingTimeMS")
     var processingTimeMS: Long? = null
 }
 
